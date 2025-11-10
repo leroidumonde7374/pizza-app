bf170806b290ebcaceac1afd7c69867f4271f60d
leroidumonde7374 <leroidumonde7374@gmail.com>
2025-11-10 14:53:13 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('pizza-cache').then(cache => {
-    return cache.addAll(['./pizza.html','./manifest.json']);
-  }));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
-});
