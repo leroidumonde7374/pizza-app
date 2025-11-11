a419ad642efcfd117129ba3f0982c3868ba5321c
leroidumonde7374 <leroidumonde7374@gmail.com>
2025-11-11 16:22:49 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,1 @@
-self.addEventListener('install', e=>{ e.waitUntil(caches.open('pizzagramme-v1').then(cache=>cache.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png']))); });
-self.addEventListener('fetch', e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
+self.addEventListener('fetch', ()=>{});
